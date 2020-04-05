eef5d0dc1dbbbcbed720431634aa9cf72924ebd3
Nikolay Yarlychenko <yarlychenkonikolay@gmail.com>
2020-04-05 22:46:59 +0300

    Bug fixed

--- src/Main.java
+++ src/Main.java
@@ -4,15 +4,7 @@ public class Main {
     public static void main(String[] args) {
         TodoListController controller;
         controller = new TodoListController();
-        try {
-            controller.runLoop();
-        } catch (IOException e) {
-            controller.saveContext();
-            e.printStackTrace();
-        } finally {
-            controller.saveContext();
-        }
-
+        controller.runLoop();
     }
 }
 
